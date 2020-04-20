665115cde41dd354f9375b4e4f9b9fda2ac540f5
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2020-04-20 17:39:24 +0100

    20th April
    -adding more detail to git users

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,101 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
-    return <div>hello</div>;
+    const {
+      name,
+      avatar_url,
+      location,
+      html_url,
+      hireable,
+      bio,
+      login,
+      company,
+      blog
+    } = this.props.user;
+    const { loading } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "200px" }}
+            />
+
+            <h1>{name}</h1>
+            <p>Location:{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit GitHub Profile
+            </a>
+
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 export default User;
